17c196437da40af7d7b2541b2536f5464c3a9d22
bibibobo <237031204+bibibobo13@users.noreply.github.com>
2026-02-28 13:49:22 +0200

    Started The Day 1 Code Only Functional Added The Task Addidng already could be used but still no .txt or .json save
    The Main.rs Code Was Updated And Added Raw Logic Without Much Terminal Ui It Will Be Fixed From 14 Day

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+//raw Code No TUI or Gui for now
+//Tasks Struct
+use std::io;
+struct Task {
+    text: String,
+    completed: bool,
+}
 fn main() {
-    println!("Hello, world!");
+    let mut tasks: Vec<Task> = Vec::new();
+    println!("Hello World XDXDDXDXD");
+    loop {
+        println!(
+            "1. For New Task \n2. For Edit Task or add a Path \n3. For Marking As Done \n4. For Deleting Task \n5. Quit"
+        );
+        let selected = shorter_input().parse().expect("BimBimBamBam");
+        match selected {
+            1 => add_task(&mut tasks),
+            2 => println!("Not Done Currently"),
+            5 => break,
+            _ => println!("Wrong Number"),
+        }
+    }
+}
+fn add_task(tasks: &mut Vec<Task>) {
+    let mut New_Task = String::new();
+    io::stdin().read_line(&mut New_Task).expect("Error");
+    let mut Maded_Task = New_Task.trim().to_string();
+    let mut Users_Task = Task {
+        text: Maded_Task,
+        completed: false,
+    };
+}
+
+fn shorter_input() -> String {
+    //here i was lazy to write it everytime
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("BimBimBamBam");
+    input.trim().to_string()
 }
