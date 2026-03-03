1a179eadc2654bcce064b5cda1937510657221cb
bibibobo <237031204+bibibobo13@users.noreply.github.com>
2026-03-03 21:58:23 +0200

    Finished Making Backend And Gui as Separate parts
    Now The Backend Works By Itself It just needs To Be Called Right Now I Using The Same Name Of Function But With _visuals at the end it means there is not backend currently the script doesnt work because the main function will be fixed tomorrow

--- src/main.rs
+++ src/main.rs
@@ -28,126 +28,61 @@ fn main() -> std::io::Result<()> {
         let selected: i32 = shorter_input(true).parse().unwrap();
         match selected {
             1 => {
-                add_task(&mut tasks);
-                let file = File::create("results.json")?;
-                let mut writer = BufWriter::new(file);
-                serde_json::to_writer(&mut writer, &tasks)?;
-                writer.flush()?;
+                add_task_visuals(&mut tasks);
+                saving(&tasks);
             }
             2 => {
                 edit_task(&mut tasks);
-                let file = File::create("results.json")?;
-                let mut writer = BufWriter::new(file);
-                serde_json::to_writer(&mut writer, &tasks)?;
-                writer.flush()?;
+                saving(&tasks);
             }
             3 => {
                 mark_done(&mut tasks);
-                let file = File::create("results.json")?;
-                let mut writer = BufWriter::new(file);
-                serde_json::to_writer(&mut writer, &tasks)?;
-                writer.flush()?;
+                saving(&tasks);
             }
             4 => {
                 delete_task(&mut tasks);
-                let file = File::create("results.json")?;
-                let mut writer = BufWriter::new(file);
-                serde_json::to_writer(&mut writer, &tasks)?;
-                writer.flush()?;
+                saving(&tasks);
             }
             5 => {
                 watch_tasks(&tasks);
-                let file = File::create("results.json")?;
-                let mut writer = BufWriter::new(file);
-                serde_json::to_writer(&mut writer, &tasks)?;
-                writer.flush()?;
+                saving(&tasks);
             }
             6 => {
-                let file = File::create("results.json")?;
-                let mut writer = BufWriter::new(file);
-                serde_json::to_writer(&mut writer, &tasks)?;
-                break writer.flush();
+                saving(&tasks);
+                break Ok(());
             }
             _ => println!("Wrong Number"),
         }
     }
 }
+fn saving(tasks: &Vec<Task>) -> std::io::Result<()> {
+    let file = File::create("results.json")?;
+    let mut writer = BufWriter::new(file);
+    serde_json::to_writer(&mut writer, &tasks)?;
+    writer.flush()
+}
 
-fn add_task(tasks: &mut Vec<Task>) {
-    let new_task = shorter_input(false).parse().unwrap();
+fn add_task(tasks: &mut Vec<Task>, text: String) {
     let users_task = Task {
-        text: new_task,
+        text: text,
         completed: false,
     };
     tasks.push(users_task);
-    println!("Added New Task");
 }
 
-fn edit_task(tasks: &mut Vec<Task>) {
-    if tasks.is_empty() {
-        println!("You Doesnt Have Anything");
-    } else {
-        println!("Heres All You Have {:?}", tasks);
-        println!("Which One You Want To Edit use only number ");
-        let i: u8 = shorter_input(true).parse().expect("BimBimBamBam");
-        let i = i - 1;
-        if i as usize >= tasks.len() {
-            println!("You Selected Wrong Task");
-            return;
-        }
-        println!("How Are you Want To Rename It");
-        let new_input = shorter_input(false);
-        let new_name = Task {
-            text: new_input,
-            completed: false,
-        };
-        tasks[i as usize] = new_name;
-    }
+fn edit_task(tasks: &mut Vec<Task>, value: u8, input: String) {
+    let new_name = Task {
+        text: input,
+        completed: false,
+    };
+    tasks[value as usize] = new_name;
 }
 
-fn mark_done(tasks: &mut Vec<Task>) {
-    if tasks.is_empty() {
-        println!("You Dont Have Tasks To mark As Done");
-        println!("You Should Create New Task");
-    } else {
-        println!(
-            "Heres All your tasks Which One You Want Mark As Done {:?}",
-            tasks
-        );
-        println!("Which One You Want To Edit use only number ");
-        let i: u8 = shorter_input(true).parse().expect("BimBimBamBam");
-        let i = i - 1;
-        println!("Are You Sure You Want To mark as Done it (Y = yes N = no)");
-        let x: String = shorter_input(false).parse().expect("BimBimBamBam");
-        if x == "Y" {
-            println!("Okay");
-            //let mut new_completion = Task.completed = true;
-            tasks[i as usize].completed = true;
-        } else {
-            println!("Okay Xd");
-        }
-    }
+fn mark_done(tasks: &mut Vec<Task>, value: u8) {
+    tasks[value as usize].completed = true;
 }
-fn delete_task(tasks: &mut Vec<Task>) {
-    if tasks.is_empty() {
-        println!("Buddy You Dont Have Anything");
-    } else {
-        println!(
-            "Heres All your tasks Which one you want to delete {:?}",
-            tasks
-        );
-        println!("Which one you wana to deleate use only numbers");
-        let i: u8 = shorter_input(true).parse().expect("BimBimBamBam");
-        let i = i - 1;
-        println!("Are You Sure You Want To Delete It Couldnt Be Redone (Y = yes N = no");
-        let x: String = shorter_input(false).parse().expect("BimBimBamBam");
-        if x == "Y" {
-            println!("Deleting");
-            tasks.remove(i as usize);
-        } else {
-            println!("Okay");
-        }
-    }
+fn delete_task(tasks: &mut Vec<Task>, value: u8) {
+    tasks.remove(value as usize);
 }
 fn watch_tasks(tasks: &Vec<Task>) {
     println!("Heres Your Tasks {:?}", tasks);
@@ -169,6 +104,74 @@ fn shorter_input(must_be_number: bool) -> String {
             } else {
                 println!("Error Wrong Input Settings Use Only Letters");
             }
+        }
+    }
+}
+fn add_task_visuals(tasks: &mut Vec<Task>) -> () {
+    println!("What The Name Of Task");
+    let text = shorter_input(false).parse().unwrap();
+    add_task(tasks, text);
+    println!("Added New Task");
+    println!("Oops Something Went Wrong");
+}
+fn edit_task_visuals(tasks: &mut Vec<Task>) -> () {
+    if tasks.is_empty() {
+        println!("You Doesnt Have Anything");
+    } else {
+        println!("Heres All You Have {:?}", tasks);
+        println!("Which One You Want To Edit use only number ");
+        let i: u8 = shorter_input(true).parse().expect("BimBimBamBam");
+        let i = i - 1;
+        if i as usize >= tasks.len() {
+            println!("You Selected Wrong Task");
+            return;
+        }
+        println!("How Are you Want To Rename It");
+        let new_input = shorter_input(false);
+        edit_task(tasks, i, new_input);
+    }
+}
+fn mark_done_visuals(tasks: &mut Vec<Task>) -> () {
+    if tasks.is_empty() {
+        println!("You Dont Have Tasks To mark As Done");
+        println!("You Should Create New Task");
+    } else {
+        println!(
+            "Heres All your tasks Which One You Want Mark As Done {:?}",
+            tasks
+        );
+        println!("Which One You Want To Edit use only number ");
+        let i: u8 = shorter_input(true).parse().expect("BimBimBamBam");
+        let i = i - 1;
+        println!("Are You Sure You Want To mark as Done it (Y = yes N = no)");
+        let x: String = shorter_input(false).parse().expect("BimBimBamBam");
+        if x == "Y" {
+            println!("Okay");
+            //let mut new_completion = Task.completed = true;
+            mark_done(tasks, i);
+        } else {
+            println!("Okay Xd");
+        }
+    }
+}
+fn delete_task_visuals(tasks: &mut Vec<Task>) -> () {
+    if tasks.is_empty() {
+        println!("Buddy You Dont Have Anything");
+    } else {
+        println!(
+            "Heres All your tasks Which one you want to delete {:?}",
+            tasks
+        );
+        println!("Which one you wana to deleate use only numbers");
+        let i: u8 = shorter_input(true).parse().expect("BimBimBamBam");
+        let i = i - 1;
+        println!("Are You Sure You Want To Delete It Couldnt Be Redone (Y = yes N = no");
+        let x: String = shorter_input(false).parse().expect("BimBimBamBam");
+        if x == "Y" {
+            println!("Deleting");
+            delete_task(tasks, i);
+        } else {
+            println!("Okay");
         }
     }
 }
